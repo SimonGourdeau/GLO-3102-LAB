9b882db0b8b1b92d4e7825a493b268439adba126
gui1837 <croteau.guillaume@hotmail.com>
2015-02-05 13:24:36 -0500

    Lab 4

--- Laboratoire 4/Calculator.js
+++ Laboratoire 4/Calculator.js
@@ -1,166 +1,143 @@
 $( document ).ready(function() {
     var calculator = new Calculator();
     var currentNumber = 0;
+    var operator;
+    var refreshNumber = false;
 
-    $("#number").click(function() {
-        alert("BAM");
-        $(".Result").val($(".Result").val() + this.value);
+    $(".number").click(function() {
+        if(refreshNumber) {
+            $("#Result").val(this.value);
+        } else {
+            $("#Result").val(this.value + $("#Result").val());
+        }
     });
 
-    $(".plus").click(function() {
-        currentNumber = $(".Result").val();
-        currentNumber = calculator.add(currentNumber);
+    $(".operand").click(function() {
+        currentNumber = $("#Result").val();
+        operator = this.value;
+        refreshNumber = true;
     });
 
-    $(".minus").click(function() {
-        currentNumber = $(".Result").val();
-        currentNumber = calculator.subtract(currentNumber);
-    });
-
-    $(".time").click(function() {
-        currentNumber = $(".Result").val();
-        currentNumber = calculator.multiply(currentNumber);
-    });
-
-    $(".divide").click(function() {
-        currentNumber = $(".Result").val();
-        currentNumber = calculator.divide(currentNumber);
-    });
-
-    $(".sin").click(function() {
-        currentNumber = $(".Result").val();
+    $("#sin").click(function() {
+        currentNumber = $("#Result").val();
         currentNumber = calculator.sin(currentNumber);
+        $("#Result").fadeOut("slow", function() {
+            $("#Result").val(currentNumber).fadeIn();
+        });
+        refreshNumber = true;
     });
 
-    $(".cos").click(function() {
-        currentNumber = $(".Result").val();
+    $("#cos").click(function() {
+        currentNumber = $("#Result").val();
         currentNumber = calculator.cos(currentNumber);
+        $("#Result").fadeOut("slow", function() {
+            $("#Result").val(currentNumber).fadeIn();
+        });
+        refreshNumber = true;
     });
 
-    $(".tan").click(function() {
-        currentNumber = $(".Result").val();
+    $("#tan").click(function() {
+        currentNumber = $("#Result").val();
         currentNumber = calculator.tan(currentNumber);
+        $("#Result").fadeOut("slow", function() {
+            $("#Result").val(currentNumber).fadeIn();
+        });
+        refreshNumber = true;
     });
 
-    $(".fact").click(function() {
-        currentNumber = $(".Result").val();
+    $("#fact").click(function() {
+        currentNumber = $("#Result").val();
         currentNumber = calculator.factorial(currentNumber);
+        $("#Result").fadeOut("slow", function() {
+            $("#Result").val(currentNumber).fadeIn();
+        });
+        refreshNumber = true;
+    });
+
+    $("#equal").click(function() {
+       switch(operator) {
+           case "+":
+               currentNumber = calculator.add(parseFloat(currentNumber), parseFloat($("#Result").val()));
+               break;
+           case "-":
+               currentNumber = calculator.substract(parseFloat(currentNumber), parseFloat($("#Result").val()));
+               break;
+           case "*":
+               currentNumber = calculator.multiply(parseFloat(currentNumber), parseFloat($("#Result").val()));
+               break;
+           case "/":
+               currentNumber = calculator.divide(parseFloat(currentNumber), parseFloat($("#Result").val()));
+               break;
+           default:
+               break;
+       }
+        $("#Result").fadeOut("slow", function() {
+            $("#Result").val(currentNumber).fadeIn();
+        });
+        refreshNumber = true;
     });
 });
 
-var str = "12/5*9+9.4*2".replace(/[^-()\d/*+.]/g, '');
+function Calculator()
+{
+    this.memorizedValue = 0;
+}
 
-var f = [];
-Math.factorial = function(n) {
+Calculator.prototype.add = function(val1 , val2)
+{
+    return (val1+val2);
+}
+
+Calculator.prototype.substract = function(val1 , val2)
+{
+    return (val1-val2);
+}
+
+Calculator.prototype.multiply = function(val1 , val2)
+{
+    return (val1*val2);
+}
+
+Calculator.prototype.divide = function(val1 , val2)
+{
+    if(val2==0) {
+        return NaN;
+    }
+    return (val1/val2);
+}
+
+Calculator.prototype.sin = function(n)
+{
+    return Math.sin(n);
+}
+
+Calculator.prototype.cos = function(n)
+{
+    return Math.cos(n);
+}
+
+Calculator.prototype.tan = function(n)
+{
+    return Math.tan(n);
+}
+
+Calculator.prototype.memorize = function(n)
+{
+    if(typeof n === 'number') {
+        this.memorizedValue = n;
+    }
+}
+
+Calculator.prototype.getMemorizedValue = function()
+{
+    return this.memorizedValue;
+}
+
+Calculator.prototype.factorial = function(n) {
     if (n == 0 || n == 1) {
         return 1;
     }
-    if (f[n] > 0) {
-        return f[n];
-    }
-    return f[n] = Math.factorial(n-1) * n;
-}
-
-// Classe Calculator
-// Toutes les méthodes sauf `equals` retournent `this`, ce qui permet de chainer les appels
-// Ex: 
-// var calculator = new Calculator()
-// calculator.add(2).add(3).subtract(2).equals()
-// Retourne : 2 
-// 2 + 3 - 2 = 2
-var Calculator = function () {
-    var memory;
-
-    var equation = '';
-
-    // Ajouter seulement une valeur à l'équation
-    // Sera utile pour lorsque cette classe sera connectée au UI
-    this.value = function(value) {
-        if(typeof value !== 'undefined'){
-            equation += parseFloat(value);
-        }
-        return this;
-    }
-
-    // Réinitialiser l'équation 
-    this.clear = function() {
-        equation = '';
-        return this;
-    }
-
-    this.add = function(value) {
-        equation += '+';
-        if(typeof value !== 'undefined'){
-            equation += parseFloat(value);
-        }
-        return this;
-    }
-
-    this.subtract = function (value) {
-        equation += '-'
-        if(typeof value !== 'undefined'){
-            equation += parseFloat(value);
-        }
-        return this;
-    }
-
-    this.multiply = function (value) {
-        equation += '*'
-        if(typeof value !== 'undefined'){
-            equation += parseFloat(value);
-        }
-        return this;
-    }
-
-    this.divide = function (value) {
-        equation += '/'
-        if(typeof value !== 'undefined'){
-            if (value == 0) {
-                throw "Division par zéro!!!";
-            }
-            equation += parseFloat(value);
-        }
-        return this;
-    }
-
-    this.sin = function(value) {
-        equation += 'Math.sin(' + parseFloat(value) + ')'
-        return this;
-    }
-
-    this.cos = function(value) {
-        equation += 'Math.cos(' + parseFloat(value) + ')'
-        return this;
-    }
-
-    this.tan = function() {
-        equation += 'Math.tan(' + parseFloat(value) + ')'
-        return this;
-    }
-
-    this.setMemory = function(memoryValue) {
-        memory = memoryValue;
-    }
-
-    this.getMemory = function() {
-        return memory;
-    }
-
-    this.factorial = function(value) {
-        if(typeof value === 'undefined'){
-            equation = 'Math.factorial(' + this.equals() + ')'
-        } else {
-            equation += 'Math.factorial(' + parseFloat(value) + ')'
-        }
-        return this;
-    }
-
-    this.equals = function () {
-        // Il faut être très prudent avec eval !!! Eval pourrait permettre d'injecter du code malicieux et l'exécuter
-        // C'est pourquoi toutes nos variables 'value' sont passées dans 'parseFloat'
-        console.log('Evaluating :', equation);
-        var equationSolution = eval(equation);
-        equation = '';
-        return equationSolution;
+    if (n > 0) {
+        return this.factorial(n-1) * n;
     }
 }
